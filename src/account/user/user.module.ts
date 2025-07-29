5f72d6f0b664c57f142fb3604f40da4962517bc2
Kimgaegeol <flying0305@naver.com>
2025-07-29 14:17:17 +0900

    module DI update

--- src/account/user/user.module.ts
+++ src/account/user/user.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { UserController } from "./presentation/controller/user.controller";
 import { UserService } from "./domain/service/user.service";
 import { UserRepository } from "./infra/user.repository";
+import { PrismaModule } from "src/common/db/prisma.module";
 
 @Module({
+    imports: [PrismaModule],
     controllers: [UserController],
     providers: [UserService, UserRepository]
 })
