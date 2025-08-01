10593464d793b2fd37075ff41751d1ec026f8dee
Kimgaegeol <flying0305@naver.com>
2025-08-01 16:43:20 +0900

    folder 구조 업데이트

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { RedisModule } from './common/db/redis.moudle';
-import { RedisServicesModule } from './common/redis/redis-services.module';
-import { TestController } from './common/test/test.controller';
+import { RedisServicesModule } from './utils/redis/redis-services.module';
+import { TestController } from './utils/test/test.controller';
 
 import { UserModule } from './account/user/user.module';
 import { AreaModule } from './site/area/area.module';
