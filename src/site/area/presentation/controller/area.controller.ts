8d6b75b22b3838e9d5adda91ce752240cd719934
Kimgaegeol <flying0305@naver.com>
2025-08-06 16:26:38 +0900

    area API 기능완성

--- src/site/area/presentation/controller/area.controller.ts
+++ src/site/area/presentation/controller/area.controller.ts
@@ -30,13 +30,13 @@ export class AreaController {
     findAll(@Query('page', new DefaultValuePipe(NUMBER_CONSTANTS.DEFAULT_PAGE), ParseIntPipe) page: number, @Query('limit', new DefaultValuePipe(NUMBER_CONSTANTS.DEFAULT_LIMIT), ParseIntPipe) limit: number) {
         return this.areaService.findAll(page, limit);
     }
+    @Get('list/:userId')
+    findListByUserId(@Param('userId', ParseIntPipe) userId: number) {
+        return this.areaService.findListByUserId(userId);
+    }
     @Get(':id')
     findOne(@Param('id', ParseIntPipe) id: number) {
         return this.areaService.findOne(id);
-    }
-    @Get('list/user/:userId')
-    findListByUserId(@Param('userId', ParseIntPipe) userId: number) {
-        return this.areaService.findListByUserId(userId);
     }
     //---UPDATE---
     @Post(':id')
@@ -44,12 +44,13 @@ export class AreaController {
         return this.areaService.updateAreaByAreaId(id, updateData);
     }
     //---DELETE---
-    @Delete(':id')
-    deleteAreaById(@Param('id', ParseIntPipe) id: number) {
-        return this.areaService.deleteAreaByAreaId(id);
-    }
     @Delete('all')
     deleteAreaALL() {
         return this.areaService.deleteAreaALL();
     }
+    @Delete(':id')
+    deleteAreaById(@Param('id', ParseIntPipe) id: number) {
+        return this.areaService.deleteAreaByAreaId(id);
+    }
+
 }
